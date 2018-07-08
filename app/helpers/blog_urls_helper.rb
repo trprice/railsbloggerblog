d3555a5dfcc2571bedfa5aebc670a40916ec3029
Taylor Price <taylor.price@gmail.com>
2018-07-07 18:41:15 -0700

    Set the blog_urls create routine to call the blog helper. Remove the blog_urls helper because it's not needed at this point and served it's purpose to help me understand how to call the blog helper.

--- app/helpers/blog_urls_helper.rb
+++ app/helpers/blog_urls_helper.rb
@@ -1,14 +1,3 @@
 # Info about the blogger api can be seen in the service.rb file here:
 module BlogUrlsHelper
-  def self.get_blog(url)
-    b_url = BlogUrl.new
-    b_url.url = url
-
-    blog = BlogsHelper.get_blogger_by_url(url)
-    b_url.blog_id = blog.blog_id
-
-    b_url.save
-
-    return b_url
-  end
 end
